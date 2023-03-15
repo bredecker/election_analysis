ba66197dbacd9bf6a09b7b78aee42412bbbfeeeb
bredecker <118203601+bredecker@users.noreply.github.com>
2023-03-14 20:44:10 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -14,7 +14,11 @@ candidate_votes = {}
 county_options = []
 county_name = []
 county_votes = {}
+#vote_percentage = (candidate_votes / total_votes) * 100
 
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Assign a variable for the file to load and the path
 #file_to_load = os.path.join("resources", "election_results.csv")
@@ -36,18 +40,46 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
 
+    for candidate_name in candidate_votes: 
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate_name} received {votes:,} votes, {vote_percentage:.1f}% of the vote.")
+        
+
         if county_name not in county_options:
             county_options.append(county_name)
             county_votes[county_name] = 0
         county_votes[county_name] += 1 
+        
 
-print(candidate_votes)
-print(county_votes)
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage): 
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+
+winning_candidate_summary = (
+        f"----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------------\n")
+print(winning_candidate_summary)
+    
+            
+
+#print(winning_candidate)
+#print(candidate_options)
+#print(candidate_votes)
+#print(county_votes)
+#print(total_votes)
+#print(vote_percentage)
 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 with open(file_to_save, "w") as txt_file:
-    election_results = f"The results are {candidate_votes}"
+    election_results = f"The results are {winning_candidate_summary}"
     txt_file.write(election_results)
 
 
